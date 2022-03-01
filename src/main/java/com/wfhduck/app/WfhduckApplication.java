92bf380cbce311d6dec74a731817816485b9d124
yinglung174 <michaelng195@gmail.com>
2022-03-01 13:16:45 +0800

    Initialization

---
+++ src/main/java/com/wfhduck/app/WfhduckApplication.java
@@ -0,0 +1,13 @@
+package com.wfhduck.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfhduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfhduckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wfhduck/app/WfhduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfhduck.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfhduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
