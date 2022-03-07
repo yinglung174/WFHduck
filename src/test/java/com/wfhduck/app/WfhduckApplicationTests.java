2b45af5a6b9e955684f6ab8015652da859424406
yinglung174 <michaelng195@gmail.com>
2022-03-07 12:51:18 +0800

    WFHDUCK-23: Basic JUnit

--- src/test/java/com/wfhduck/app/WfhduckApplicationTests.java
+++ src/test/java/com/wfhduck/app/WfhduckApplicationTests.java
@@ -1,13 +1,22 @@
 package com.wfhduck.app;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wfhduck.app.controller.HomeController;
 
 @SpringBootTest
 class WfhduckApplicationTests {
 
+	@Autowired
+	private HomeController controller;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
